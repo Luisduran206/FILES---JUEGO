f48da7bcdb71e7abe5abf62cd30012cf7ca253d5
Luisduran206 <112398598+Luisduran206@users.noreply.github.com>
2023-03-20 00:04:00 -0600

    Update MainPersona.java

--- MainPersona.java
+++ MainPersona.java
@@ -1,11 +1,8 @@
 import java.io.IOException;
-import java.util.Scanner;
 public class MainPersona {
 
 	public static void main(String[] args) throws IOException {
 		principal crear = new principal();
-		//System.out.println("Ingrese su nombre de usuario para actualizar sus datos o generar un nuevo jugador: ");
-		
 		crear.generarPersonajes();
 	}
 }
